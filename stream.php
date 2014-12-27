42830ef3135da057b0a842058d8e06a3eca385c1
Pavel Dimitriev <pavel.dimitriev@paymentwall.com>
2014-12-27 23:31:17 +0200

    added stream.php

---
+++ stream.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'config.php';
+
+$service = stream_socket_server("tcp://0.0.0.0:5682", $errno, $errstr);
+
+if (!$service) {
+    echo "$errstr ($errno)\n";
+} else {
+    $src = fopen(PW_INPUT_STREAM, 'r');
+
+    while ($conn = stream_socket_accept($service)) {
+        stream_copy_to_stream($src, $conn);
+        fclose($conn);
+    }
+
+    fclose($service);
+}
+
