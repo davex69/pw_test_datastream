5a155cb8ca3756b5d29b486805d17c0454344eb6
Pavel Dimitriev <pavel.dimitriev@paymentwall.com>
2014-12-27 23:31:03 +0200

    added config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('PW_PROJECT_ROOT', dirname(__FILE__));
+define('PW_INPUT_STREAM', PW_PROJECT_ROOT . '/data/stream');
+
--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
 require_once 'vendor/autoload.php';
+require_once 'config.php';
 
 //ini_set('memory_limit', '1M');
-
-define('PW_PROJECT_ROOT', dirname(__FILE__));
-define('PW_INPUT_STREAM', PW_PROJECT_ROOT . '/data/stream');
 
 (new Paymentwall\DataFilter)->search();
 
