332818196fe37dfe7bfd200f24eaf1791fc3132e
Pavel Dimitriev <pavel.dimitriev@paymentwall.com>
2014-12-27 22:23:13 +0200

    added memory limit restriction

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require_once 'vendor/autoload.php';
 
+//ini_set('memory_limit', '1M');
+
 define('PW_PROJECT_ROOT', dirname(__FILE__));
 define('PW_INPUT_STREAM', PW_PROJECT_ROOT . '/data/stream');
 
